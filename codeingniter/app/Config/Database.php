6970499cc00dcc7d446e1b1e27df350d007aacd0
Octavio07 <84403783+Octavio07@users.noreply.github.com>
2021-06-16 14:15:34 -0500

    Update Database.php

--- codeingniter/app/Config/Database.php
+++ codeingniter/app/Config/Database.php
@@ -33,9 +33,9 @@ class Database extends Config
 	public $default = [
 		'DSN'      => '',
 		'hostname' => 'localhost',
-		'username' => '',
-		'password' => '',
-		'database' => '',
+		'username' => 'octavio',
+		'password' => 'sastre2222',
+		'database' => 'miniproyecto',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
